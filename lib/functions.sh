bf029017d62e2063fe5d9d1b2c84b09ec49fd778
Zaharia Constantin <layout.webdesign@gmail.com>
2021-07-30 12:07:38 +0300

    Fix some sh notation

--- lib/functions.sh
+++ lib/functions.sh
@@ -115,22 +115,23 @@ docker_rmi_tag() {
 # by placing a .venv file in the project root with a virtualenv name in it
 function workon_cwd {
     # Check that this is a Git repo
-    GIT_DIR=`git rev-parse --git-dir 2> /dev/null`
+    GIT_DIR=$(git rev-parse --git-dir 2> /dev/null)
     if [ $? == 0 ]; then
         # Find the repo root and check for virtualenv name override
-        GIT_DIR=`\cd $GIT_DIR; pwd`
-        PROJECT_ROOT=`dirname "$GIT_DIR"`
-        ENV_NAME=`basename "$PROJECT_ROOT"`
+        GIT_DIR=$(cd "$GIT_DIR"|| pwd)
+        PROJECT_ROOT=$(dirname "$GIT_DIR")
+        ENV_NAME=$(basename "$PROJECT_ROOT")
         if [ -f "$PROJECT_ROOT/.venv" ]; then
-            ENV_NAME=`cat "$PROJECT_ROOT/.venv"`
+            ENV_NAME=$(cat "$PROJECT_ROOT/.venv")
         fi
         # Activate the environment only if it is not already active
         if [ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]; then
             if [ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]; then
-                workon "$ENV_NAME" && export CD_VIRTUAL_ENV="$ENV_NAME"
+              echo Activated virtualenv "${ENV_NAME}".
+              workon "${ENV_NAME}" && export CD_VIRTUAL_ENV="$ENV_NAME"
             fi
         fi
-    elif [ $CD_VIRTUAL_ENV ]; then
+    elif [ "${CD_VIRTUAL_ENV}" ]; then
         # We've just left the repo, deactivate the environment
         # Note: this only happens if the virtualenv was activated automatically
         deactivate && unset CD_VIRTUAL_ENV
